cd0e1c671597c482bce1bec4688eaeeb686e2da3
natan.fagundes <natan.fags@gmail.com>
2023-02-03 02:22:40 -0300

    configurado bd e carga inicial para testes

---
+++ src/main/resources/data.sql
@@ -0,0 +1,17 @@
+insert into veiculo (id, nome, codigo)
+values (1, 'carro', 111);
+
+insert into veiculo (id, nome, codigo)
+values (2, 'bicicleta', 222);
+
+insert into veiculo (id, nome, codigo)
+values (3, 'moto', 333);
+
+insert into veiculo (id, nome, codigo)
+values (4, 'caminhao', 444);
+
+insert into veiculo (id, nome, codigo)
+values (5, 'barco', 555);
+
+insert into veiculo (id, nome, codigo)
+values (6, 'aviao', 666);
